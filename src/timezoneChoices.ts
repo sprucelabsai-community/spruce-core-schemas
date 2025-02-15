7edb04006d763b1b7d476149a4c7858e255fef26
Taylor Romero <me@taylorrome.ro>
2025-02-15 12:14:43 -0700

    patch: added zones that don't support dst

--- src/timezoneChoices.ts
+++ src/timezoneChoices.ts
@@ -2,14 +2,32 @@ export const timezoneChoices = [
     // UTC
     { value: 'UTC', label: 'UTC' },
 
-    // North America
+    // Americas
     { value: 'Pacific/Honolulu', label: 'Hawaii Time' },
     { value: 'America/Anchorage', label: 'Alaska Time' },
     { value: 'America/Los_Angeles', label: 'Pacific Time' },
     { value: 'America/Denver', label: 'Mountain Time' },
+
+    // No-DST Mountain
+    { value: 'America/Phoenix', label: 'Phoenix Time (No DST)' },
+
     { value: 'America/Chicago', label: 'Central Time' },
+
+    // No-DST Central
+    { value: 'America/Guatemala', label: 'Central Standard Time (No DST)' },
+
     { value: 'America/New_York', label: 'Eastern Time' },
+
+    // No-DST Eastern
+    { value: 'America/Panama', label: 'Eastern Standard Time (No DST)' },
+
     { value: 'America/Sao_Paulo', label: 'Brasília Time' },
+
+    // No-DST in South America
+    {
+        value: 'America/Argentina/Buenos_Aires',
+        label: 'Argentina Time (No DST)',
+    },
 
     // Europe
     { value: 'Europe/London', label: 'UK Time' },
@@ -37,4 +55,7 @@ export const timezoneChoices = [
     { value: 'Australia/Adelaide', label: 'Central Australia Time' },
     { value: 'Australia/Sydney', label: 'Eastern Australia Time' },
     { value: 'Pacific/Auckland', label: 'New Zealand Time' },
+
+    // No-DST in Oceania
+    { value: 'Pacific/Port_Moresby', label: 'Papua New Guinea Time (No DST)' },
 ] as const
